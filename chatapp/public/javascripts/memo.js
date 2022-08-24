95546989698d0046938d962ed0ce8472b01c37d6
kide-stu <eipamu.syun@gmail.com>
2022-08-24 14:45:19 +0900

    memoの修正

--- chatapp/public/javascripts/memo.js
+++ chatapp/public/javascripts/memo.js
@@ -3,11 +3,21 @@
 // メモを画面上に表示する
 function memo() {
     // ユーザ名を取得
-    const userName = $('#userName').text()
+    const userName = $('#userName').text();
     // 入力されたメッセージを取得
     const message = $('#message').val();
+
+
+
     // メモの内容を表示
     $('#thread').prepend('<p>' + userName + 'さんのメモ : ' + message + '</p>');
+
+    /*//時間の取得
+    require('date-utils');
+    let dt = new Date();
+    let formatted = dt.toFormat("YYYY/MM//DD HH24時MI分SS秒");*/
+
+
 
     return false;
 }
