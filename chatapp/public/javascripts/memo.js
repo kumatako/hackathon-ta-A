e014c13aee4a5d9d339385f48f124fcc34339119
kide-stu <eipamu.syun@gmail.com>
2022-08-24 17:21:29 +0900

    空スペースをmemoできないようにした

--- chatapp/public/javascripts/memo.js
+++ chatapp/public/javascripts/memo.js
@@ -6,16 +6,26 @@ function memo() {
     const userName = $('#userName').text();
     // 入力されたメッセージを取得
     const message = $('#message').val();
+    //メッセージのスペースを除去
+    const nospacemessage = message.replace(/\s+/g, "");
+    //メッセージの改行を除去
+    const nokaigyoumessage = nospacemessage.replace(/\n+/g, "");
 
 
-
-    // メモの内容を表示
-    $('#thread').prepend('<p>' + userName + 'さんのメモ : ' + message + '</p>');
+    //メッセージが空でないか判別
+    if (nokaigyoumessage !== ""){
+        // メモの内容を表示
+        $('#thread').prepend('<p>' + userName + 'さんのメモ : ' + message + '</p>');
+    }
+    else {
+        alert("投稿文を入力してください");
+    }
 
     /*//時間の取得
     require('date-utils');
-    let dt = new Date();
-    let formatted = dt.toFormat("YYYY/MM//DD HH24時MI分SS秒");*/
+    const dt = new Date();
+    const formatted = dt.toFormat("YYYY/MM/DD HH24時MI分SS秒");
+    console.log(formatted);*/
 
 
 
