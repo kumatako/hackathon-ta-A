5e080c9d1661dcf69da2793eb54ec49b9c825a98
kide-stu <eipamu.syun@gmail.com>
2022-08-30 17:12:48 +0900

    改行、スペースを投稿に反映できるようにしました

--- chatapp/public/javascripts/publish.js
+++ chatapp/public/javascripts/publish.js
@@ -5,11 +5,15 @@ function publish() {
     // ユーザ名を取得
     const userName = $('#userName').val();
     // 入力されたメッセージを取得
-    const message = $('#message').val();
+    let message = $('#message').val();
     //メッセージのスペースを除去
     const nospacemessage = message.replace(/\s+/g, "");
     //メッセージの改行を除去
     const nokaigyoumessage = nospacemessage.replace(/\n+/g, "");
+    //メッセージの改行をタグに書き換え
+    message = message.replace(/\n+/g, "<br>");
+    //メッセージのスペースをタグに書き換え
+    message = message.replace(/\s+/g, "&ensp;");
     // 投稿内容を送信
     //メッセージが空でないかの判別
     if (nokaigyoumessage !== "") {
