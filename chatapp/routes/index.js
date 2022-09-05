68a19733e27be61e216709a35aee617aaedadb52
toyamanya <ryotoyamaiii@gmail.com>
2022-09-06 02:59:59 +0900

    データベースファイルとsignupページの作成

--- chatapp/routes/index.js
+++ chatapp/routes/index.js
@@ -14,12 +14,15 @@ router.post('/room', function (request, response, next) {
     console.log('ユーザ名：' + request.body.userName);
     const userIcon = request.body['index-icon-select'];
     console.log('選択したアイコン：' + userIcon);
-    response.render('room', { userName: request.body.userName,userIcon:userIcon});
+    response.render('room', { userName: request.body.userName, userIcon: userIcon });
 
 });
 
 
-
+// ログイン画面の表示
+router.get('/signup', function (request, response, next) {
+    response.render('signup');
+})
 
 
 
