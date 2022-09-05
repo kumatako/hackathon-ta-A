a7e5cdeda131f8b1dddd4de6b7a2635f3ba7b157
kide-stu <eipamu.syun@gmail.com>
2022-09-05 23:26:15 +0900

    全ての投稿にアイコンをつけた

--- chatapp/sockets/publish.js
+++ chatapp/sockets/publish.js
@@ -7,13 +7,13 @@ module.exports = function (socket, io) {
     // ソケットから投稿を受信
     // receiveMessageEvent
     // 全部のソケットに送信する
-    socket.on('sendMessageEvent', function (message,userName) {
+    socket.on('sendMessageEvent', function (message,userName,Icon) {
         
     //時刻の取得
     require('date-utils');
     let dt = new Date();
     let formatted = dt.toFormat("YYYY/MM/DD HH24時MI分SS秒");
 
-        io.sockets.emit('receiveMessageEvent',message,userName,formatted);
+        io.sockets.emit('receiveMessageEvent',message,userName,formatted,Icon);
     });
 };
