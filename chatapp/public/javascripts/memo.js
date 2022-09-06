324d7d8464a4cd69f58c1ada30bc0337c2752427
kide-stu <eipamu.syun@gmail.com>
2022-09-06 16:33:53 +0900

    スタンプの背景を透過,メモ機能の修正

--- chatapp/public/javascripts/memo.js
+++ chatapp/public/javascripts/memo.js
@@ -7,6 +7,7 @@ function memo() {
 
     //ユーザ名を取得
     const userName = $('#userName').val();
+    const myIcon = $('#userIcon').val();
     
     // 入力されたメッセージを取得
     const message = $('#message').val();
@@ -19,7 +20,7 @@ function memo() {
     //メッセージが空でないか判別
     if (nokaigyoumessage !== ""){
         // メモの内容を表示
-        $('#thread').prepend('<p>' + userName + 'さんのメモ : ' + message +'</p>');
+        $('#thread').prepend('<p> <img src="' + myIcon + '"alt=""style="width: 45px;height: 45px;border-radius: 22.5px;border: 1px solid lightgray;margin-right: 10px;">私のメモ:&ensp;' + message +'</p>');
         // テキストボックスの初期化
         $('#message').val('');
     }
