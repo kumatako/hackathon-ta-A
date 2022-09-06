3f9f6fc09c6e5216591c384fff2471695504ddee
kide-stu <eipamu.syun@gmail.com>
2022-09-06 16:23:52 +0900

    スタンプに日付を追加

--- chatapp/sockets/stamp.js
+++ chatapp/sockets/stamp.js
@@ -2,6 +2,9 @@
 
 module.exports = function(socket, io) {
     socket.on('sendStampEvent', function(stamp, userName, Icon) {
-        io.sockets.emit('receiveStampEvent', stamp, userName, Icon);
+        require('date-utils');
+        let dt = new Date();
+        let formatted = dt.toFormat("HH24:MI");
+        io.sockets.emit('receiveStampEvent', stamp, userName, Icon, formatted);
     });
 };
