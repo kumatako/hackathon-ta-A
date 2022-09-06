7d604c99580069ec1433b209f7fd29effa4235fa
kide-stu <eipamu.syun@gmail.com>
2022-09-06 18:48:45 +0900

    @で囲んだユーザ名にダイレクトメッセージを送れるようにしました

--- chatapp/public/javascripts/publish.js
+++ chatapp/public/javascripts/publish.js
@@ -8,6 +8,21 @@ function publish() {
     const Icon = $('#userIcon').val();
     // 入力されたメッセージを取得
     let message = $('#message').val();
+
+
+    //@の位置の取得
+    const search = '@';
+    const flag = -1;
+    let toName = "";
+    let search2 = message.indexOf(search);
+    let search3 = message.lastIndexOf(search);
+    if (flag == search2 || flag == search3) {
+    }
+    else {
+        toName = message.substring(search2 +1, search3 );
+        message = message.substring(search3+1);
+    }
+
     //メッセージのスペースを除去
     const nospacemessage = message.replace(/\s+/g, "");
     //メッセージの改行を除去
@@ -16,11 +31,13 @@ function publish() {
     message = message.replace(/\n+/g, "<br>");
     //メッセージのスペースをタグに書き換え
     message = message.replace(/\s+/g, "&ensp;");
+
+
     // 投稿内容を送信
     //メッセージが空でないかの判別
     if (nokaigyoumessage !== "") {
         // sendMessageEvent
-        socket.emit('sendMessageEvent', message, userName, Icon);
+        socket.emit('sendMessageEvent', message, userName, Icon, toName);
         $('#message').val('');
     }
 
@@ -33,15 +50,29 @@ function publish() {
 }
 
 // サーバから受信した投稿メッセージを画面上に表示する
-socket.on('receiveMessageEvent', function (message, userName, formatted,Icon) {
+socket.on('receiveMessageEvent', function (message, userName, formatted,Icon, toName) {
 
     const myUserName = $('#userName').val();
     const myIcon = $('#userIcon').val();
-    if (myUserName === userName && myIcon == Icon) {
-        $('#thread').prepend('<div class="mymessage">&ensp;<small><'+formatted+'></small>&ensp;<div class="message">'+message+'</div><div class="name-and-icon"><img class="icon" src="'+myIcon+'">私</div></div>');
-    } else {
-        $('#thread').prepend('<div class="othersmessage"><div class="name-and-icon"><img class="icon" src="'+Icon+'">'+userName+'</div><div class="message">'+message+'</div>&ensp;<small><'+formatted+'></small>&ensp;</div>');
+    if (toName == '') {
+        if (myUserName === userName && myIcon == Icon) {
+            $('#thread').prepend('<div class="mymessage">&ensp;<small><'+formatted+'></small>&ensp;<div class="message">'+message+'</div><div class="name-and-icon"><img class="icon" src="'+myIcon+'">私</div></div>');
+        } else {
+            $('#thread').prepend('<div class="othersmessage"><div class="name-and-icon"><img class="icon" src="'+Icon+'">'+userName+'</div><div class="message">'+message+'</div>&ensp;<small><'+formatted+'></small>&ensp;</div>');
+        }
+
     }
+    else if (toName == myUserName || userName == myUserName) {
+        if (myUserName === userName && myIcon == Icon) {
+            $('#thread').prepend('<div class="mymessage">&ensp;<small><'+formatted+'></small>&ensp;<div class="message">(ダイレクトメッセージ)'+message+'</div><div class="name-and-icon"><img class="icon" src="'+myIcon+'">私</div></div>');
+        } else {
+            $('#thread').prepend('<div class="othersmessage"><div class="name-and-icon"><img class="icon" src="'+Icon+'">'+userName+'</div><div class="message">(ダイレクトメッセージ)'+message+'</div>&ensp;<small><'+formatted+'></small>&ensp;</div>');
+        }
+    }
+    else {
+
+    }
+
 });
 
 
--- chatapp/sockets/publish.js
+++ chatapp/sockets/publish.js
@@ -7,13 +7,13 @@ module.exports = function (socket, io) {
     // ソケットから投稿を受信
     // receiveMessageEvent
     // 全部のソケットに送信する
-    socket.on('sendMessageEvent', function (message,userName,Icon) {
+    socket.on('sendMessageEvent', function (message,userName,Icon,toName) {
         
     //時刻の取得
     require('date-utils');
     let dt = new Date();
     let formatted = dt.toFormat("HH24:MI");
 
-        io.sockets.emit('receiveMessageEvent',message,userName,formatted,Icon);
+        io.sockets.emit('receiveMessageEvent',message,userName,formatted,Icon,toName);
     });
 };
